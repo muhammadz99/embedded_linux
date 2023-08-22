9e7ec59c18ea656d795de095d14bc5094fab68f3
muhammadz99 <muhammad_abdelaziz99@hotmail.com>
2023-08-22 09:27:31 +0300

    Embedded LInux Diploma
    Week2/Task02
    
    Task is about Script that generate a text of Activity for you to do
    The Script using url request method and parsing the needed file to get the Activity

---
+++ 01python/tasks/week2/task02.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+#Write a code to suggest automatically activates for you
+import requests
+
+x = requests.get('https://www.boredapi.com/api/activity')
+print(x.json()['activity'])
