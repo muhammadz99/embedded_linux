2df228fe6b6e74b1b347040e3ef5d871ea0ac4e7
muhammadz99 <muhammad_abdelaziz99@hotmail.com>
2023-08-27 03:54:14 +0300

    Embdded Linux Diploma/Python Course Week02/#Task13
    #Python program to get the ASCII value of a character (using ord())

---
+++ 01python/tasks/week2/task13.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+
+#Python program to get the ASCII value of a character (using ord())
+print(ord('a'))
+print(ord('3'))
+print(ord('%'))
+print(ord('x'))
+print(ord('P'))
+print(ord('b'))
+print(ord('-'))
